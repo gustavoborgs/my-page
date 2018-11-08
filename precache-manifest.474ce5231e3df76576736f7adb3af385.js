82ba657fdd1ae3b9017e1fc5c15389cdee5118e0
gustavocavalcante <gustavo.cavalcante.borges@gmail.com>
2018-11-08 13:35:56 -0200

    Updates

---
+++ precache-manifest.474ce5231e3df76576736f7adb3af385.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d65eeecbc6dae33d2a8a",
+    "url": "/my-page/static/js/runtime~main.d65eeecb.js"
+  },
+  {
+    "revision": "33f6a49ebad2d6bf26b8",
+    "url": "/my-page/static/js/main.33f6a49e.chunk.js"
+  },
+  {
+    "revision": "82ad78dcef013575efe2",
+    "url": "/my-page/static/js/1.82ad78dc.chunk.js"
+  },
+  {
+    "revision": "33f6a49ebad2d6bf26b8",
+    "url": "/my-page/static/css/main.08e15683.chunk.css"
+  },
+  {
+    "revision": "c9914ab61fb78ffd73e76313eb957273",
+    "url": "/my-page/index.html"
+  }
+];
